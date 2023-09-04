12014c862736072bc04ec0feec0bddfbc6d0beac
AbdulWahabAbbasi <awa8877@gmail.com>
2023-09-04 15:55:59 +0500

    Adding daptiveFeatureSelectionLayer and change LSTM to GRU with epoch 20

--- models/layers.py
+++ models/layers.py
@@ -96,27 +96,64 @@ class conv_block(nn.Module):
     def forward(self,x):
         x = self.conv(x)
         return x
-        
+
+##################################################################daptiveFeatureSelectionLayer
+import torch.nn as nn
 class SqueezeAttentionBlock(nn.Module):
-    def __init__(self, ch_in, ch_out):
+    def __init__(self, ch_in, ch_out, intermediate_channels=None):
         super(SqueezeAttentionBlock, self).__init__()
-        self.avg_pool = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.conv = conv_block(ch_in, ch_out)
-        self.conv_atten = conv_block(ch_in, ch_out)
-        self.upsample = nn.Upsample(scale_factor=2)
+
+        if intermediate_channels is None:
+            intermediate_channels = ch_out // 2  # default to half of ch_out
+
+        self.weight_network = nn.Sequential(
+            nn.Conv2d(ch_in, intermediate_channels, kernel_size=1),
+            nn.BatchNorm2d(intermediate_channels),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(intermediate_channels, intermediate_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(intermediate_channels),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(intermediate_channels, ch_out, kernel_size=1),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(ch_out, ch_out, kernel_size=3, padding=1),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(ch_out, ch_out, kernel_size=1),
+            nn.BatchNorm2d(ch_out),
+            nn.Sigmoid()
+        )
 
     def forward(self, x):
-        #print("x.shape: ", x.shape)
-        x_res = self.conv(x)
-        #print("x_res.shape: ", x_res.shape)
-        y = self.avg_pool(x)
-        #print("y.shape dopo avg pool: ", y.shape)
-        y = self.conv_atten(y)
-        #print("y.shape dopo conv att:", y.shape)
-        y = self.upsample(y)
-        #print(y.shape, x_res.shape)
-        #print("(y * x_res) + y: ", (y * x_res) + y)
-        return (y * x_res) + y
+        weights = self.weight_network(x)
+        return x * weights
+
+
+##################################################################SqueezeAttentionBlock
+# class SqueezeAttentionBlock(nn.Module):
+#     def __init__(self, ch_in, ch_out):
+#         super(SqueezeAttentionBlock, self).__init__()
+#         self.avg_pool = nn.AvgPool2d(kernel_size=2, stride=2)
+#         self.conv = conv_block(ch_in, ch_out)
+#         self.conv_atten = conv_block(ch_in, ch_out)
+#         self.upsample = nn.Upsample(scale_factor=2)
+#
+#     def forward(self, x):
+#         #print("x.shape: ", x.shape)
+#         x_res = self.conv(x)
+#         #print("x_res.shape: ", x_res.shape)
+#         y = self.avg_pool(x)
+#         #print("y.shape dopo avg pool: ", y.shape)
+#         y = self.conv_atten(y)
+#         #print("y.shape dopo conv att:", y.shape)
+#         y = self.upsample(y)
+#         #print(y.shape, x_res.shape)
+#         #print("(y * x_res) + y: ", (y * x_res) + y)
+#         return (y * x_res) + y
 
 def center_crop(layer, max_height, max_width):
     _, _, h, w = layer.size()
